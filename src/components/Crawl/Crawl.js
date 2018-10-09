c8a13654925dc5422906de5a3f6de3a8561e72ab
Kevin Simpson <relasine@gmail.com>
2018-10-09 15:20:39 -0600

    Add scrolling text and styling

--- src/components/Crawl/Crawl.js
+++ src/components/Crawl/Crawl.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types';
 
 
 const Crawl = ({film}) => {
+  const romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
   return (
     <aside>
-      <h1 className="crawl-title">{film.title}</h1>
-      <p className="crawl-text">{film.opening_crawl}</p>
-      <p className="crawl-date">{film.release_date}</p>
+      <section className="crawl-container">
+        <div className="crawl-header">
+          <h1 className="crawl-episode">EPISODE {romanNumerals[film.episode_id -1]}</h1>
+          <h1 className="crawl-title">{film.title}</h1>
+        </div>
+        <p className="crawl-text">{film.opening_crawl}</p>
+        <p className="crawl-date">{film.release_date}</p>
+      </section>
     </aside>
   );
 }
