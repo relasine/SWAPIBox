7970b0d6cc1995ba8c2988724bbe3dfb37d52511
Kevin Simpson <relasine@gmail.com>
2018-10-12 07:37:01 -0600

    Add test list

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,84 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+
+  });
+
+  const defaultState = {
+    totalFavorites: 0,
+    currentSelection: '',
+    openingCrawl: {},
+    people: [],
+    vehicles: [],
+    planets: [],
+  };
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have default state', () => {
+    expect(wrapper.state()).toEqual(defaultState);
+  });
+
+  it('should call crawlCall() on componentDidMount', () => {
+
+  });
+
+  it('should call fetch when crawlCall is called', () => {
+
+  });
+
+  it('should set state when crawlCallis called', () => {
+
+  });
+
+  it('should call callFetchPeople if people is currentSelection', () => {
+
+  });
+
+  it('should call callFetchVehicles if vehicles is currentSelection', () => {
+
+  });
+
+  it('should call callFetchPlanets if planets is currentSelection', () => {
+
+  });
+
+  it('should call fetchVehicles if callFetchVehicles is called', () => {
+
+  });
+
+  it('should set state after callFetchVehicles is called', () => {
+
+  });
+
+  it('should call fetchPeople if callFetchPeople is called', () => {
+
+  });
+
+  it('should set state after callFetchPeople is called', () => {
+
+  });
+
+  it('should call fetchPlanets if callFetchPlanets is called', () => {
+
+  });
+
+  it('should set state after callFetchPlanets is called', () => {
+
+  });
+
+
+})
