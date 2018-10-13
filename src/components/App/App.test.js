a0b88c33c1662a59bf59e54c7615699476e58946
Kevin Simpson <relasine@gmail.com>
2018-10-12 18:24:02 -0600

    Add app api testing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -60,7 +60,7 @@ describe('App', () => {
   it('should set state when crawlCall is called', async () => {
     const mockFetch = jest.fn(() => { return {
       results: [{film: 'test'}, {film: 'test'}], count: 2
-    }})
+    }});
     wrapper.state().fetchCall = mockFetch
     await wrapper.instance().crawlCall()
 
@@ -79,28 +79,67 @@ describe('App', () => {
 
   });
 
-  it('should call fetchVehicles if callFetchVehicles is called', () => {
+  it('should call fetchVehicles if callFetchVehicles is called', async () => {
+    const mockFetch = jest.fn();
+    const mockVehicleClass = {fetchVehicles: mockFetch}
+    wrapper.state().fetchVehicles = mockVehicleClass;
 
+    await wrapper.instance().callFetchVehicles();
+    expect(mockFetch).toHaveBeenCalled();
   });
 
-  it('should set state after callFetchVehicles is called', () => {
+  it('should set state after callFetchVehicles is called', async () => {
+    const mockFetch = jest.fn(() => {
+      return [{vehicle: 'car'}, {vehicle: 'boat'}]
+    });
+    const mockVehicleClass = {fetchVehicles: mockFetch}
+    wrapper.state().fetchVehicles = mockVehicleClass;
+    
+    await wrapper.instance().callFetchVehicles();
 
+    expect(wrapper.state().vehicles).toEqual([{vehicle: 'car'}, {vehicle: 'boat'}])
   });
 
-  it('should call fetchPeople if callFetchPeople is called', () => {
+  it('should call fetchPeople if callFetchPeople is called', async () => {
+    const mockFetch = jest.fn();
+    const mockPeopleClass = {fetchPeople: mockFetch}
+    wrapper.state().fetchPeople = mockPeopleClass;
 
+    await wrapper.instance().callFetchPeople();
+    expect(mockFetch).toHaveBeenCalled();
   });
 
-  it('should set state after callFetchPeople is called', () => {
+  it('should set state after callFetchPeople is called', async () => {
+    const mockFetch = jest.fn(() => {
+      return [{person: 'joe'}, {person: 'sara'}]
+    });
+    const mockPeopleClass = {fetchPeople: mockFetch}
+    wrapper.state().fetchPeople = mockPeopleClass;
+    
+    await wrapper.instance().callFetchPeople();
 
+    expect(wrapper.state().people).toEqual([{person: 'joe'}, {person: 'sara'}])
   });
 
-  it('should call fetchPlanets if callFetchPlanets is called', () => {
+  it('should call fetchPlanets if callFetchPlanets is called', async () => {
+    const mockFetch = jest.fn();
+    const mockPlanetsClass = {fetchPlanets: mockFetch}
+    wrapper.state().fetchPlanets = mockPlanetsClass;
 
+    await wrapper.instance().callFetchPlanets();
+    expect(mockFetch).toHaveBeenCalled();
   });
 
-  it('should set state after callFetchPlanets is called', () => {
+  it('should set state after callFetchPlanets is called', async () => {
+    const mockFetch = jest.fn(() => {
+      return [{planet: 'earth'}, {planet: 'mars'}]
+    });
+    const mockPlanetClass = {fetchPlanets: mockFetch}
+    wrapper.state().fetchPlanets = mockPlanetClass;
+    
+    await wrapper.instance().callFetchPlanets();
 
+    expect(wrapper.state().planets).toEqual([{planet: 'earth'}, {planet: 'mars'}])
   });
 
 
