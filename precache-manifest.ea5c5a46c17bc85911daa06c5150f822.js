f5ab006d8d621eaf55fd47901ec8ea35afbcc897
Kevin Simpson <relasine@gmail.com>
2018-10-17 11:58:12 -0600

    Updates

---
+++ precache-manifest.ea5c5a46c17bc85911daa06c5150f822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ceead34fe2838c7fd14",
+    "url": "/SWAPIBox/static/js/runtime~main.3ceead34.js"
+  },
+  {
+    "revision": "b799db8c6954cf1eab7d",
+    "url": "/SWAPIBox/static/js/main.b799db8c.chunk.js"
+  },
+  {
+    "revision": "7d8e5854acae16b82cc4",
+    "url": "/SWAPIBox/static/js/1.7d8e5854.chunk.js"
+  },
+  {
+    "revision": "b799db8c6954cf1eab7d",
+    "url": "/SWAPIBox/static/css/main.173bfb2c.chunk.css"
+  },
+  {
+    "revision": "427ebf7f5e677cfea8b196813fdbd647",
+    "url": "/SWAPIBox/index.html"
+  }
+];
