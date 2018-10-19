2019dfdfd677e278c77abc2641362f0484315ba4
Kevin Simpson <relasine@gmail.com>
2018-10-19 09:05:14 -0600

    Make hamburger close on mediaquery selection

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,80 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Hamburger.css';
 
-class Hamburger extends Component {
-  constructor() {
-    super()
+const Hamburger = ({ hamburgerChange, status }) => {
+  return(
+    <section 
+      className='lightsaburger'
+      onClick={() => {
+        hamburgerChange()
+      }}
+    >
+      <div className={`top ${status.topToggled}`}>
+       <div className={`top-blade blade ${status.topBladeToggled}`}></div>
+       <div className={`top-grip grip ${status.gripToggled}`}></div>
+      </div>
+      <div className={`middle ${status.middleToggled}`}>
+        <div className={`middle-grip grip ${status.gripToggled}`}></div>
+        <div className={`middle-blade blade ${status.middleBladeToggled}`}></div>
+      </div>
+      <div className={`bottom ${status.bottomToggled}`}>
+       <div className={`bottom-blade blade ${status.bottomBladeToggled}`}></div>
+       <div className={`bottom-grip grip ${status.gripToggled}`}></div>
+      </div>
 
-    this.state = {
-      topToggled: '',
-      middleToggled: '',
-      bottomToggled: '',
-      topBladeToggled: '',
-      middleBladeToggled: '',
-      bottomBladeToggled: '',
-      gripToggled: '',
-      login: 'hide-login'
-    }
-  }
-
-  handleHamburger = () => {
-    if (this.state.topToggled === '' && this.props.ready) {
-      this.setState({
-        topToggled: 'top-toggled',
-        middleToggled: 'middle-toggled',
-        bottomToggled: 'bottom-toggled',
-        topBladeToggled: 'top-blade-toggled',
-        middleBladeToggled: 'middle-blade-toggled',
-        bottomBladeToggled: 'bottom-blade-toggled',
-        gripToggled: 'grip-toggled',
-        login: 'hide-login'
-      })
-      this.props.hamburgerChange();
-    } else if (this.props.ready) {
-      this.setState({
-        topToggled: '',
-        middleToggled: '',
-        bottomToggled: '',
-        topBladeToggled: '',
-        middleBladeToggled: '',
-        bottomBladeToggled: '',
-        gripToggled: '',
-        login: 'hide-login'
-      });
-      this.props.hamburgerChange()
-    } else {
-      this.props.loginWarning()
-    }
-  }
-
-  render() {
-    return(
-      <section 
-        className='lightsaburger'
-        onClick={() => {
-          this.handleHamburger()
-        }}
-      >
-        <div className={`top ${this.state.topToggled}`}>
-         <div className={`top-blade blade ${this.state.topBladeToggled}`}></div>
-         <div className={`top-grip grip ${this.state.gripToggled}`}></div>
-        </div>
-        <div className={`middle ${this.state.middleToggled}`}>
-          <div className={`middle-grip grip ${this.state.gripToggled}`}></div>
-          <div className={`middle-blade blade ${this.state.middleBladeToggled}`}></div>
-        </div>
-        <div className={`bottom ${this.state.bottomToggled}`}>
-         <div className={`bottom-blade blade ${this.state.bottomBladeToggled}`}></div>
-         <div className={`bottom-grip grip ${this.state.gripToggled}`}></div>
-        </div>
-
-      </section>
-    )
-  }
+    </section>
+  )
 }
 
 Hamburger.propTypes = {
+  status: PropTypes.object.isRequired,
   hamburgerChange: PropTypes.func.isRequired,
   loginWarning: PropTypes.func.isRequired,
   ready: PropTypes.bool.isRequired
