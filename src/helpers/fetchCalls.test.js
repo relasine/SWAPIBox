efaebaf79e0dbe6260c1d35328df3866f7c0516e
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-21 10:46:37 -0600

    lint all files in helpers folder

--- src/helpers/fetchCalls.test.js
+++ src/helpers/fetchCalls.test.js
@@ -6,13 +6,13 @@ describe('fetchCall', () => {
 
   beforeEach(() => {
     mockUrl = ''
-  })
+  });
 
   it('should call fetch with the correct params', async () => {
     window.fetch = jest.fn().mockImplementation(() =>
       Promise.resolve({json: () => Promise.resolve('returned Data')})
-    )
-    await fetchCall(mockUrl)
-    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
-  })
+    );
+    await fetchCall(mockUrl);
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+  });
 })
